55417869da40295ae9c89cc86942cbdb5e6bf8d6
Pierre Talbot <pierre.talbot@uni.lu>
2023-11-29 10:30:54 +0100

    Add a simple file to update all github repositories.

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+projects=("cuda-battery" "lala-core" "lala-parsing" "lala-power" "lala-pc" "turbo" "lattice-land.github.io" ".github")
+
+for project in ${projects[@]}; do
+  cd ../${project}
+  git pull
+done
